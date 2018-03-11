72da8ae8d0470d29d34684d9d721a111b4bd1a77
Mats Töpel <mats@topel.se>
2018-03-11 14:27:37 +0100

    Working version

--- radman.py
+++ radman.py
@@ -9,26 +9,47 @@ import argparse
 
 parser = argparse.ArgumentParser(prog=sys.argv[0], description="ADD A DESCRIPTION OF YOUR PROGRAM HERE.")
 parser.add_argument(dest="input_file", help="Input file")
+parser.add_argument("--het_loci", help="Outputs the names of the heerozygous sites for each individual")
 parser.add_argument("-v", "--verbose", action="store_true", help="Be more verbose")
 args = parser.parse_args()
 
 class Individual(object):
-	def __init__(self, name):
+	def __init__(self, name, locus_names, het_states):
 		self.name = name
+		self.locus_names = locus_names
+		self.het_states = het_states
+	
+	# Allele states
+	# 0: Missing data
+	# 1: Homozygous
+	# 2: Heterozygous
 
-	def something(self):
-		pass
+	def get_het_locus(self):
+		het_loci = []
+		pos = 0
+		for state in self.het_states:
+			if int(state) == 2:
+				het_loci.append(self.locus_names[pos])
+			pos += 1
+		return het_loci
+
+	def get_state(self, locus_number):
+		return self.het_states[locus_number]
+
+	def __str__(self):
+		return self.name
 
 
 def main():
 	
 	data = open(args.input_file)
+	individuals = []
 
 	# Generate a list of Locus names
 	# and subsequently store the data from
 	# each individual in an object.
 	first_line = True
-	locus = []
+	locus_names = []
 	for line in data.readlines():
 		if first_line:
 			for locus_name in line.split(","):
@@ -40,14 +61,39 @@ def main():
 					name = name
 					sufix = "_b"
 				locus_name = name + sufix
-				# Skip first columndd
+				# Skip first column
 				if locus_name == "_a":
 					pass
 				else:
-					locus.append(locus_name)
+					locus_names.append(locus_name)
 			first_line = False
-	print(locus)
+		else:
+			# 'het' for heterozygosity state 
+			individual = True
+			het_states = []
+			for het in line.split(","):
+				if individual:
+					individual_name = het
+					individual = False
+				else:
+					het_states.append(het)
+			# Store each individual in a list
+			individuals.append(Individual(individual_name, locus_names, het_states))
 
+
+	print(locus_names)
+#	print(het_states)
+#	for ind in individuals:
+#		print(ind, ind.get_het_locus())
+#	print(individuals)
+
+#	locus_number = 0
+#	for locus in locus_names:
+#		print(locus)
+#		state = individuals[locus_number].get_state(locus_number)
+#		if int(state) == "2":
+#			print(state)
+#		locus_number += 1
 
 if __name__ == "__main__":
     main()
